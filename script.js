de6bd7a2931a662eb8c693bab50c4b7cdfb14ece
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-18 01:43:48 +0100

    Fixed reset

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const containerDiv = document.querySelector(".container");
 console.log(containerDiv);
 
 
-function makeGrid(){
-    for(let i = 0; i < 16; i++){
+function makeGrid(gridItems = 16){
+    for(let i = 0; i < gridItems; i++){
         const temp = document.createElement("div");
         temp.classList.add("gridItem", "gridHead");
 
 
         containerDiv.appendChild(temp);
 
-        for(let j = 0; j < 16; j++){
+        for(let j = 0; j < gridItems; j++){
             const tempChild = document.createElement("div");
             tempChild.classList.add("gridItem");
             temp.appendChild(tempChild);
@@ -31,7 +31,21 @@ resetButton.addEventListener("click", e => {
     const gridItemList = containerDiv.getElementsByClassName("gridItem");
     const gridItemArray= Array.from(gridItemList);
 
+    let userReply = 0;
+
+    do{
+        userReply = +prompt("What should the number of grids be? It has to be a number and below 100!");
+        console.log(typeof userReply + userReply);
+    }
+    while(typeof userReply != "number" || userReply > 100);
+        
+    
+
     gridItemArray.forEach(gridItem => {
         gridItem.classList.remove("blue");
     });
+
+    containerDiv.innerHTML = "";
+
+    makeGrid(userReply);
 })
